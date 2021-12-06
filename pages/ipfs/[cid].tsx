95ffdc9335e9bc3df3f9d189c1677aa2b61b57c2
RemiIO <i@remi.email>
2021-12-06 16:06:59 +0800

    delete if before loading page.

--- pages/ipfs/[cid].tsx
+++ pages/ipfs/[cid].tsx
@@ -8,30 +8,29 @@ const IPFSViewer: any = () => {
   const router = useRouter();
   const { cid } = router.query;
 
-  return (cid) ? (
-    <div>
-      <Head>
-        <title>meta-network-data-viewer</title>
-        <meta name="description" content="meta-network-data-viewer" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header className=" bg-purple-900">
-        <div className="mx-auto max-w-6xl p-4">
-          <h1 className="text-2xl font-thin text-white">meta-network-data-viewer</h1>
-        </div>
-      </header>
-      <DataViewer options={
-        {
-          platform: 'ipfs',
-          id: cid as string,
-          idName: 'CID',
-          dataSourceName: 'IPFS Gateway',
-          dataSourceList: ipfsGatewayList,
-          timeout: 20000
-        }
-      } />
-    </div>
-  ) : <>CID ERROR</>
+  return <div>
+    <Head>
+      <title>meta-network-data-viewer</title>
+      <meta name="description" content="meta-network-data-viewer" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <header className=" bg-purple-900">
+      <div className="mx-auto max-w-6xl p-4">
+        <h1 className="text-2xl font-thin text-white">meta-network-data-viewer</h1>
+      </div>
+    </header>
+    <DataViewer options={
+      {
+        platform: 'ipfs',
+        id: cid as string,
+        idName: 'CID',
+        dataSourceName: 'IPFS Gateway',
+        dataSourceList: ipfsGatewayList,
+        timeout: 20000
+      }
+    } />
+  </div>
 }
+
 
 export default IPFSViewer;
