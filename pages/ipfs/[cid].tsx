86637dc6fbb426e43938f9b3319630e5b38a2c47
RemiIO <i@remi.email>
2021-12-06 15:26:44 +0800

    delete 'getInitialProps' for Page.

--- pages/ipfs/[cid].tsx
+++ pages/ipfs/[cid].tsx
@@ -1,32 +1,37 @@
 import Head from 'next/head';
 import ipfsGatewayList from '../../ipfs-gateway.json';
 import DataViewer from '../../components/DataViewer';
+import { useRouter } from 'next/router';
 
-const IPFSViewer: any = (props: { cid: string, ok: boolean }) => (props.ok) ? (
-  <div>
-    <Head>
-      <title>meta-network-data-viewer</title>
-      <meta name="description" content="meta-network-data-viewer" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <header className=" bg-purple-900">
-      <div className="mx-auto max-w-6xl p-4">
-        <h1 className="text-2xl font-thin text-white">meta-network-data-viewer</h1>
-      </div>
-    </header>
-    <DataViewer options={
-      {
-        platform: 'ipfs',
-        id: props.cid,
-        idName: 'CID',
-        dataSourceName: 'IPFS Gateway',
-        dataSourceList: ipfsGatewayList,
-        timeout: 20000
-      }
-    } />
-  </div>
-) : <>CID ERROR</>
+const IPFSViewer: any = () => {
 
-IPFSViewer.getInitialProps = async ({ query }) => query.cid ? { cid: query.cid, ok: true } : { cid: '', ok: false }
+  const router = useRouter();
+  const { cid } = router.query;
+
+  return (cid) ? (
+    <div>
+      <Head>
+        <title>meta-network-data-viewer</title>
+        <meta name="description" content="meta-network-data-viewer" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className=" bg-purple-900">
+        <div className="mx-auto max-w-6xl p-4">
+          <h1 className="text-2xl font-thin text-white">meta-network-data-viewer</h1>
+        </div>
+      </header>
+      <DataViewer options={
+        {
+          platform: 'ipfs',
+          id: cid as string,
+          idName: 'CID',
+          dataSourceName: 'IPFS Gateway',
+          dataSourceList: ipfsGatewayList,
+          timeout: 20000
+        }
+      } />
+    </div>
+  ) : <>CID ERROR</>
+}
 
 export default IPFSViewer;
