c4f4ecca97da424ee5158f0fa88cf69e6538da74
RemiIO <i@remi.email>
2022-02-11 19:55:53 +0800

    import @metaio/meta-signature-util-v2 for test.

--- utils/metaSignature.ts
+++ utils/metaSignature.ts
@@ -1,8 +1,10 @@
 import { AuthorPostDigestMetadata, authorPostDigestSign, authorPostDigest } from '@metaio/meta-signature-util';
 
-export * from '@metaio/meta-signature-util';
+// export * from '@metaio/meta-signature-util';
+export * from '@metaio/meta-signature-util-v2';
 
 export type AuthorDigestMetadata = AuthorPostDigestMetadata;
 export const authorDigestSign = authorPostDigestSign;
 export const authorDigest = authorPostDigest;
+
 
