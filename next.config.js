96ab76ec430bd7246fb1c6175b230b1d4bc29d1a
RemiIO <i@remi.email>
2022-02-15 19:52:51 +0800

    update config for redirect.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/arweave/tx/:hash*',
+        destination: '/arweave/:hash*',
+        permanent: true,
+      },
+    ]
+  },
+  // exportPathMap: async function (
+  //   defaultPathMap,
+  //   { dev, dir, outDir, distDir, buildId }
+  // ) {
+  //   return {
+  //     '/': { page: '/' },
+  //     '/about': { page: '/about' },
+  //     '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
+  //     '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
+  //     '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
+  //   }
+  // },
 }
